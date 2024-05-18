7f9f641a6095d2903ab25c2c84435d970aabb6cd
karasb1 <bohdankaras456@gmail.com>
2024-05-18 16:04:50 +0300

    The ex4 was done.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,27 @@ using namespace std;
 
 int main()
 {
-
-
-	return 0;
+    const int size = 12;
+    int arr[size];
+    for (size_t i = 0; i < size; i++)
+    {
+        cout << "Enter the number: ";
+        cin >> arr[i];
+    }
+    int min = arr[0];
+    int max = arr[0];
+    for (size_t i = 0; i < size; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    cout << "Min: " << min << endl;
+    cout << "Max: " << max << endl;
+    return 0;
 }
