54e036701de7235e08d41433a0c87f3a29a29f2f
karasb1 <bohdankaras456@gmail.com>
2024-05-18 16:15:15 +0300

    The ex2 was done.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,32 @@ using namespace std;
 
 int main()
 {
-
-
-	return 0;
+    const int size = 12;
+    int arr[size];
+    for (size_t i = 0; i < size; i++)
+    {
+        cout << "Enter the number: ";
+        cin >> arr[i];
+    }
+    int a, b;
+    cout << "Enter a range:";
+    cin >> a;
+    cout << "Enter a range:";
+    cin >> b;
+    int min = arr[a];
+    int max = arr[a];
+    for (size_t i = a-1; i < b; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    cout << "Min: " << min << endl;
+    cout << "Max: " << max << endl;
+    return 0;
 }
