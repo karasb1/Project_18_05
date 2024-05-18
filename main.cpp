5b1470ca1ae95fe37a41e4cb8e8e769f24f7f3cc
karasb1 <bohdankaras456@gmail.com>
2024-05-18 16:00:13 +0300

    The ex3 was done.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,18 @@ using namespace std;
 
 int main()
 {
-
-
-	return 0;
+    const int full_memory_size = 5;
+    int arr[full_memory_size];
+    int sum = 0;
+    for (size_t i = 0; i < full_memory_size; i++)
+    {
+        cout << "Enter the number: ";
+        cin >> arr[i];
+    }
+    for (size_t i = 0; i < full_memory_size; i++)
+    {
+        sum += arr[i];
+    }
+    cout << sum << endl;
+    return 0;
 }
