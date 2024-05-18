fe42dd05d72401e98909b8bce6f8163e386135e2
karasb1 <bohdankaras456@gmail.com>
2024-05-18 16:10:54 +0300

    The ex1 was done.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
-int main()
-{
 
+int main() {
+    srand(time(0));
+    const int size = 10;
+    int arr[size];
+    for (size_t i = 0; i < size; i++)
+    {
+        arr[i] = rand() % 201 - 100;
+    }
+    for (size_t i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    int min = arr[0];
+    int max = arr[0];
+    for (size_t i = 0; i < size; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    cout << "Min: " << min << endl;
+    cout << "Max: " << max << endl;
 
-	return 0;
+    return 0;
 }
